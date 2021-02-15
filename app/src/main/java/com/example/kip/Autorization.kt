28ac9669f4b8c612605585df59dd2c13add0a3c3
JackHemming <kovalov.dmytriy@gmail.com>
2021-02-15 22:59:49 +0200

    Created "My Cabinet" page

--- app/src/main/java/com/example/kip/Autorization.kt
+++ app/src/main/java/com/example/kip/Autorization.kt
@@ -1,11 +1,19 @@
 package com.example.kip
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Autorization : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_autorization)
+        val button = findViewById<Button>(R.id.autorizate)
+
+        button.setOnClickListener {
+            val intent = Intent(this, Main_screen::class.java)
+            startActivity(intent)
+        }
     }
 }
